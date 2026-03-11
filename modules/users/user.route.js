00de05a71a88ecee01f4799087239d642f539a6b
SadipBhattarai <sadipbhattarai3@gmail.com>
2026-03-11 13:09:00 +0545

    Added register and login fu nctions

--- modules/users/user.route.js
+++ modules/users/user.route.js
@@ -1,3 +1,20 @@
 const router = require("express").Router();
+const userController = require("./user.controller");
 
-module.exports= router;
+router.post("/register", async (req, res, next) => {
+    try {
+        const result = await userController.register(req.body);
+        res.json({ data: "User registerd Sucessfully" })
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.post("/login", async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error)
+    }
+});
+module.exports = router;
